95b891068c40d45b735737a7c2a3f7d3d195ae9b
Sudhanshu Patel <t_sudhanshu.patel@tatadigital.com>
2023-07-17 14:30:36 +0530

    example package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='weather_pkg',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/sudhanshuptl/weather_pkg.git',
+    author='Sudhanshu Patel',
+    author_email='sudhanshuptl13@gmail.com',
+    license='unlicense',
+    packages=['weather_pkg'],
+    zip_safe=True
+)
---
+++ weather_pkg/__init__.py
@@ -0,0 +1,1 @@
+from .myfunc import my_cool_test_method
